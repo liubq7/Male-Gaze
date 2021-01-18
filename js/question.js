05a19b81d6c5769daf255afb3dbd90220ea8d8e4
liubq7 <liubq97@gmail.com>
2021-01-18 15:12:21 +0800

    jquery

--- js/question.js
+++ js/question.js
@@ -4,27 +4,6 @@ $("#nextBtn").click(() => {
     localStorage.setItem("ipt", ipt);
     location.href = "./selfview.html";
   } else {
-    $("#errorText").css({display: "block"});
+    $("#errorText").css({ display: "block" });
   }
-})
-
-
-
-// var nextBtn=document.getElementById('nextBtn');
-// var camera = window.localStorage.getItem('camera')
-
-// nextBtn.onclick=function(){
-//     var ipt=document.getElementById('ipt').value;
-//     console.log(ipt)
-//     if(ipt){
-//         window.localStorage.setItem('val',ipt);
-//         location.href="./selfview.html";
-//         if (camera == "on") {          
-//             window.localStorage.setItem('look',"./assets/selfview/look-camera.gif");
-//          } else {
-//             window.localStorage.setItem('look',"./assets/selfview/look-black.gif");
-//          }
-//     }else{
-//         showError();
-//     }
-// }
+});
--- js/selfview.js
+++ js/selfview.js
@@ -1,27 +1,17 @@
+var mode = localStorage.getItem("mode");
+var ipt = localStorage.getItem("ipt");
+var camera = document.querySelector("#camera");
 
+$("#word").text(ipt);
 
-
-var video = document.querySelector("#video-camera");
-var camera = window.localStorage.getItem('camera')
-
-if(camera == "on")
-{if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true })
+if (mode === "gaze") {
+  $("#look").attr("src", "./assets/selfview/look-camera.gif");
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
     .then(function (stream) {
-      video.srcObject = stream;
+      camera.srcObject = stream;
     })
-    .catch(function (err0r) {
-      console.log("Something went wrong!");
+    .catch(function (err) {
+      console.log(err);
     });
-}}
-
-
-var val=window.localStorage.getItem('ipt'),
-    p=document.getElementById('word');
-    p.innerText=val;
-
-var lookSrc = window.localStorage.getItem('look');
-var lookImg = document.getElementById('look');
-
-lookImg.setAttribute("src",lookSrc);
-
+}
